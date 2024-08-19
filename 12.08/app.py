352c655bb346dc923a385618082e8764c1affb94
MrSamix <103048493+MrSamix@users.noreply.github.com>
2024-08-19 19:29:45 +0300

    Update app.py

--- 12.08/app.py
+++ 12.08/app.py
@@ -3,7 +3,6 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from dotenv import find_dotenv, load_dotenv
-import requests
 import os
 
 load_dotenv(find_dotenv())
